3120ab026296b9f3acf1d34b040dcdc3b6747096
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-12 23:45:37 +0300

    consuming quote API

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,30 @@
 from flask import render_template, redirect, url_for,abort,request
 from . import main
+from app.requests import get_quote
 from flask_login import login_required,current_user
-from .. import db
+from ..models import User,Quote
+from ..import db
 
+import os
+
+
+from ..email import mail_message
+
+
+
+#Views
 @main.route('/')
 def index():
-    """
-    views
-    """
+    '''
+    main
+    '''
     return render_template('index.html')
+@main.route('/quotes')
+def quotes():
+
+    '''
+    '''
+    quote = get_quote()
+    title = 'LetsBlog | Quotes'
+    
+    return render_template('quotes.html', title = title,quote = quote)
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 
-from app.models import User
+from app.models import Quote, User
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -24,7 +24,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Quote=Quote)
 
 
 if __name__ == '__main__':
