540380cbe35fbd7298f7db38708fe5cab944873c
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-13 15:24:11 +0300

    adding prifileupdating route

--- app/main/views.py
+++ app/main/views.py
@@ -17,3 +17,20 @@ def index():
     quote = get_quote()
     blogs = Blog.query.order_by(Blog.time.desc())
     return render_template('index.html', blogs=blogs,quote = quote)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    user = User.query.filter_by(username = name).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+        db.session.commit()
+        return redirect(url_for('main.profile',name=user.username,))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    return render_template('profile/update.html', user = user, form =form)
