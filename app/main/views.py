977d6a98ebda623c28b1ad74de7c7595b585c513
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-13 15:22:42 +0300

    updating my main.index

--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,19 @@
-from flask import render_template, redirect, url_for,abort,request
+from . import render_template, redirect,url_for,abort,request
 from . import main
 from app.requests import get_quote
 from flask_login import login_required,current_user
-from ..models import User,Quote
-from ..import db
-
+from ..models import Role,User,Blog,Comment,Subscriber
+from ..import db, photos
+import secrets
 import os
-
-
+from PIL import Image
+from .forms import UpdateProfile,CreateBlog,CommentForm
 from ..email import mail_message
-
 
 
 #Views
 @main.route('/')
 def index():
     quote = get_quote()
-    return render_template('index.html',quote = quote)
+    blogs = Blog.query.order_by(Blog.time.desc())
+    return render_template('index.html', blogs=blogs,quote = quote)
