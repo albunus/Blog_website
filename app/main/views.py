d99cdc6fc9b6818e0316b3fa7c922c6ea49e0899
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-13 15:31:49 +0300

    adding a coment method

--- app/main/views.py
+++ app/main/views.py
@@ -90,3 +90,12 @@ def updateblog(blog_id):
         form.description.data = blog.description
         form.content.data = blog.content
     return render_template('edit_blog.html', form = form)
+
+@main.route('/comment/<blog_id>', methods = ['Post','GET'])
+@login_required
+def comment(blog_id):
+    blog = Blog.query.get(blog_id)
+    comment =request.form.get('newcomment')
+    new_comment = Comment(comment = comment, user_id = current_user._get_current_object().id, blog_id=blog_id)
+    new_comment.save()
+    return redirect(url_for('main.blog',id = blog.id))
