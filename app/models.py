5e1ca8274c79745eff6e88d0f150163fe02794fe
albunus <albunus.nyalita@student.moringaschool.com>
2021-11-13 14:59:17 +0300

    adding subscriber class

--- app/models.py
+++ app/models.py
@@ -98,3 +98,15 @@ class Quote:
   def __init__(self, author, quote):
     self.author = author
     self.quote = quote
+    
+class Subscriber(db.Model):
+  __tablename__='subscribers'
+  id=db.Column(db.Integer,primary_key=True)
+  email = db.Column(db.String(255),unique=True,index=True)
+
+  def save_subscriber(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'Subscriber {self.email}'
